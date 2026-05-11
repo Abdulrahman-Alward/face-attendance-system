ca2545cb717d293cc79a62c2db5d0397624c04b2
Abdulrahman Alward <ab2003ward.am@gmail.com>
2026-05-11 11:06:24 +0300

    edit the run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 @echo off
-REM Launches the Streamlit app using the system Python where face_recognition is installed.
-"C:\Users\Award\anaconda3\envs\cv2" -m streamlit run "%~dp0app.py"
+REM Launches the Streamlit app using the `cv2` conda env, where all the
+REM project dependencies (dlib + CUDA, torch + CUDA, facenet-pytorch,
+REM insightface, streamlit, ...) are installed.
+"C:\Users\Award\anaconda3\envs\cv2\python.exe" -m streamlit run "%~dp0app.py"
